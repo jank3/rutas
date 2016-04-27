46b0fe77dcd03e01c4ece33996ee9be387a3e556
corradopablom <corrado.pablom@gmail.com>
2016-04-27 14:36:08 -0300

    Primer commit

---
+++ ConsoleApplication1/Tacho.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Tacho
+    {
+        public int id { get; set; }
+        public int distancia { get; set; }
+        public bool estoyLleno { get; set; }
+    }
+}
